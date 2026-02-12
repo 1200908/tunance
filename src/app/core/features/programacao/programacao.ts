1b9f1df006578762230f9f12e14cf5d55d7baf21
1200908 <1200908@isep.ipp.pt>
2026-02-12 23:52:49 +0000

    Navegation between navbar upgrade to stay more smooth and always go to Top and button on program changed to point to Guide instead of home

--- src/app/core/features/programacao/programacao.ts
+++ src/app/core/features/programacao/programacao.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import {Router, RouterLink, RouterLinkActive} from '@angular/router';
 
 
 @Component({
@@ -9,5 +9,15 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   styleUrl: './programacao.css',
 })
 export class ProgramacaoComponent {
+
+  constructor(private router: Router) {}
+
+  goToGuia() {
+    this.router.navigate(['/guia-turistico']).then(() => {
+      const el = document.querySelector('.guia-hero');
+      if (el) el.scrollIntoView({ behavior: 'smooth' });
+    });
+  }
+
 
 }
--- src/app/core/layout/navbar/navbar.ts
+++ src/app/core/layout/navbar/navbar.ts
@@ -1,14 +1,16 @@
 import { Component, HostListener } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import {Router, RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
 export class NavbarComponent {
+  constructor(private router: Router) {}
+
   isNavbarHidden = false;
   lastScrollTop = 0;
 
@@ -23,5 +25,39 @@ export class NavbarComponent {
       this.isNavbarHidden = false;
     }
     this.lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+  }
+
+  goToHome() {
+    this.router.navigate(['/home']).then(() => {
+      const el = document.querySelector('.hero');
+      if (el) el.scrollIntoView({ behavior: 'smooth' });
+    });
+  }
+  goToAbout() {
+    this.router.navigate(['/about']).then(() => {
+      const el = document.querySelector('.about-hero');
+      if (el) el.scrollIntoView({ behavior: 'smooth' });
+    });
+  }
+
+  goToProgramacao() {
+    this.router.navigate(['/programacao']).then(() => {
+      const el = document.querySelector('.programacao-hero');
+      if (el) el.scrollIntoView({ behavior: 'smooth' });
+    });
+  }
+
+  goToGuia() {
+    this.router.navigate(['/guia-turistico']).then(() => {
+      const el = document.querySelector('.guia-hero');
+      if (el) el.scrollIntoView({ behavior: 'smooth' });
+    });
+  }
+
+  goToContact() {
+    this.router.navigate(['/contact']).then(() => {
+      const el = document.querySelector('.contact-hero');
+      if (el) el.scrollIntoView({ behavior: 'smooth' });
+    });
   }
 }
